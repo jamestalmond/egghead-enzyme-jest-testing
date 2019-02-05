8fc856b59ef06fecc031db444a316fd2b00db3fc
James Almond <jamestalmond@gmail.com>
2019-02-05 09:27:13 +0000

    Fully rendering components with the mount method

--- src/App.test.js
+++ src/App.test.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App, { Link } from './App';
 
-import { configure, shallow } from 'enzyme';
+import { configure, shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import toJson from 'enzyme-to-json';
 
 configure({ adapter: new Adapter() });
 
-describe('<App />', () => {
+describe('<App /> shallow rendering', () => {
 	it('should render 1 p tag', () => {
 		const wrapper = shallow(<App />);
 		expect(wrapper.find('p').length).toBe(1);
@@ -47,6 +47,56 @@ describe('<App />', () => {
 	it('matches the snapshot', () => {
 		const tree = shallow(<App />);
 		expect(toJson(tree)).toMatchSnapshot();
+	});
+});
+
+describe('<App /> mount rendering', () => {
+	it('should render 1 p tag', () => {
+		const wrapper = mount(<App />);
+		expect(wrapper.find('p').length).toBe(1);
+		wrapper.unmount();
+	});
+
+	it('should render an element with the class .App-header', () => {
+		const wrapper = mount(<App />);
+		expect(wrapper.find('.App-header').exists()).toBe(true);
+		wrapper.unmount();
+	});
+
+	it('should render an ul tag with 3 children', () => {
+		const wrapper = mount(<App />);
+		expect(wrapper.find('ul').children().length).toBe(3);
+		wrapper.unmount();
+	});
+
+	it('should render an ul tag with the className App-list', () => {
+		const wrapper = mount(<App />);
+		expect(wrapper.find('ul').hasClass('App-list')).toBe(true);
+		wrapper.unmount();
+	});
+
+	it('should render an a tag with the text Learn React', () => {
+		const wrapper = mount(<App />);
+		expect(wrapper.find('a').text()).toBe('Learn React');
+		wrapper.unmount();
+	});
+
+	it('should find an element with the text prop', () => {
+		const wrapper = mount(<App />);
+		expect(wrapper.find('[text="Foo"]'));
+		wrapper.unmount();
+	});
+
+	it('should find an element with the specified alt text', () => {
+		const wrapper = mount(<App />);
+		expect(wrapper.find({ alt: 'logo' }));
+		wrapper.unmount();
+	});
+
+	it('matches the snapshot', () => {
+		const tree = mount(<App />);
+		expect(toJson(tree)).toMatchSnapshot();
+		tree.unmount();
 	});
 });
 
