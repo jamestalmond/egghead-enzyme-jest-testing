d25d8e33c6e1f5b2ca304991d1d40ba21bc7614e
James Almond <jamestalmond@gmail.com>
2019-02-05 09:08:17 +0000

    Enzyme snapshots

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,7 @@ import App from './App';
 
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
 
 configure({ adapter: new Adapter() });
 
@@ -41,5 +42,10 @@ describe('<App />', () => {
 	it('should find an element with the specified alt text', () => {
 		const wrapper = shallow(<App />);
 		expect(wrapper.find({ alt: 'logo' }));
+	});
+
+	it('matches the snapshot', () => {
+		const tree = shallow(<App />);
+		expect(toJson(tree)).toMatchSnapshot();
 	});
 });
