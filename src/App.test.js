7afc86e162e75f339a0448107b4c21bfea2dedfc
James Almond <jamestalmond@gmail.com>
2019-02-05 09:20:24 +0000

    Testing with component props

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App, { Link } from './App';
 
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -47,5 +47,24 @@ describe('<App />', () => {
 	it('matches the snapshot', () => {
 		const tree = shallow(<App />);
 		expect(toJson(tree)).toMatchSnapshot();
+	});
+});
+
+describe('<Link />', () => {
+	it('link component accepts address prop', () => {
+		const wrapper = shallow(<Link address="www.google.com" />);
+		expect(wrapper.instance().props.address).toBe('www.google.com');
+	});
+
+	it('a tag node renders href correctly', () => {
+		const wrapper = shallow(<Link address="www.google.com" />);
+		expect(wrapper.props().href).toBe('www.google.com');
+	});
+
+	it('returns null with true hide prop ', () => {
+		const wrapper = shallow(<Link hide={false} />);
+		expect(wrapper.find('a').length).toBe(1);
+		wrapper.setProps({ hide: true });
+		expect(wrapper.get(0)).toBeNull();
 	});
 });
